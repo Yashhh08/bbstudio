9a93802a853382c6434960d723028c450c550ce6
Yash Yerunkar <yashyerunkar8@gmail.com>
2024-09-17 12:27:39 +0530

    refactor: updated popup video

--- components/shared/MetroCoach.tsx
+++ components/shared/MetroCoach.tsx
@@ -6,6 +6,15 @@ import metroCoach from "@/public/assets/images/metro-coach.png";
 import { Button } from "../ui/button";
 import { Maximize2, X } from "lucide-react";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 interface Props {
   brand: string;
   videos: { id: string; src: string }[];
@@ -49,106 +58,38 @@ const MetroCoach = (props: Props) => {
             <source src={video.src} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
-          <Button
-            size="icon"
-            variant="secondary"
-            className="absolute bottom-2 right-2"
-            onClick={() => handleEnlarge(video.id)}
-          >
-            <Maximize2 className="h-7 w-7 z-1 bg-white p-1 rounded-lg" />
-          </Button>
+
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button
+                size="icon"
+                variant="secondary"
+                className="absolute bottom-2 right-2 rounded-3xl"
+                onClick={() => handleEnlarge(video.id)}
+              >
+                <Maximize2 className="h-7 w-7 z-1 bg-white p-1 rounded-lg" />
+              </Button>
+            </DialogTrigger>
+
+            <DialogContent className="border-none">
+              <video
+                autoPlay
+                loop
+                controls
+                className="w-full h-full rounded-xl"
+              >
+                <source
+                  src={
+                    props.videos.find((v) => v.id === enlargedVideo)?.src || ""
+                  }
+                  type="video/mp4"
+                />
+                Your browser does not support the video tag.
+              </video>
+            </DialogContent>
+          </Dialog>
         </div>
       ))}
-
-      {enlargedVideo && (
-        <div className="bg-opacity-75 z-50 left-[50%] translate-x-[-50%] w-[100vw]">
-          <div className="relative w-[75vw]">
-            <video autoPlay loop controls className="w-full h-full">
-              <source
-                src={
-                  props.videos.find((v) => v.id === enlargedVideo)?.src || ""
-                }
-                type="video/mp4"
-              />
-              Your browser does not support the video tag.
-            </video>
-            <Button
-              size="icon"
-              variant="secondary"
-              className="absolute top-2 right-2"
-              onClick={handleClose}
-            >
-              <X className="h-4 w-4" />
-            </Button>
-          </div>
-        </div>
-      )}
-
-      {/* <div>
-        <video
-          width={270}
-          autoPlay
-          loop
-          muted
-          className="absolute rounded-3xl left-[6%] top-[15%]"
-        >
-          <source
-            src={
-              "https://cdn.shopify.com/videos/c/o/v/0215a395de3f41c3987a38fcf0f243a7.mp4"
-            }
-            type="video/mp4"
-          />
-          Your browser does not support the video tag.
-        </video>
-
-        <video
-          width={270}
-          autoPlay
-          loop
-          muted
-          className="absolute rounded-3xl left-[25%] top-[15%]"
-        >
-          <source
-            src={
-              "https://cdn.shopify.com/videos/c/o/v/0215a395de3f41c3987a38fcf0f243a7.mp4"
-            }
-            type="video/mp4"
-          />
-          Your browser does not support the video tag.
-        </video>
-
-        <video
-          width={270}
-          autoPlay
-          loop
-          muted
-          className="absolute rounded-3xl right-[6%] top-[15%]"
-        >
-          <source
-            src={
-              "https://cdn.shopify.com/videos/c/o/v/0215a395de3f41c3987a38fcf0f243a7.mp4"
-            }
-            type="video/mp4"
-          />
-          Your browser does not support the video tag.
-        </video>
-
-        <video
-          width={270}
-          autoPlay
-          loop
-          muted
-          className="absolute rounded-3xl right-[25%] top-[15%]"
-        >
-          <source
-            src={
-              "https://cdn.shopify.com/videos/c/o/v/0215a395de3f41c3987a38fcf0f243a7.mp4"
-            }
-            type="video/mp4"
-          />
-          Your browser does not support the video tag.
-        </video>
-      </div> */}
     </div>
   );
 };
